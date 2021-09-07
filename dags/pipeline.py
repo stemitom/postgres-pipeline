95351b75eea4acdf37f19468e000d82bbbd4a23c
Wasp IX <stemitom@gmail.com>
2021-09-07 12:01:27 +0100

    Refactoring

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -7,7 +7,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from plugins.operators.csv_to_postgres import LoadCsvtoPostgresOperator
+from operators.csv_to_postgres import LoadCsvtoPostgresOperator
 from requests import exceptions
 
 args = {
